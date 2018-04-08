4824c43088c7f1260e3ac73c94ebf6b1eac0a8ec
geotechdev <aitabdelmalekma@gmail.com>
2018-04-08 20:41:48 +0100

    Finir avec les roches métamorphiques

--- docs/app/objects.js
+++ docs/app/objects.js
@@ -9,13 +9,10 @@ var perso = {
             this.force + " en force";
     }
 };
-
 console.log("\n" + perso.decrire());
-
-perso.sante = perso.sante - 20;
-perso.force = perso.force + 10;
-perso.xp = perso.xp + 15;
-
+perso.sante -= 20;
+perso.force += 10;
+perso.xp += 15;
 console.log(perso.decrire());
 
 
